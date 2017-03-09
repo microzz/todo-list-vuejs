1faa97ac9ae16dc11afb867686155f7b335c4aa9
Zhaohui <zhaohui@microzz.com>
2017-03-09 21:36:47 +0800

    upload

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # todo-list-vuejs
 A simple todo list demo by Vue.js
+# vue-todo-list
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+
---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1 v-text="text"></h1>
+      <input type="text" v-model="newItem" v-on:keyup.enter="addNew" />
+      <button v-on:click="addNew">Add</button>
+      <ul>
+        <li v-for="item in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggle(item)">{{item.label}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Hello from './components/Hello'
+import Store from './store';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      text: 'Todo List',
+      items: Store.fetch(),
+      newItem: ''
+    }
+  },
+  watch: {
+    items: {
+      handler(items) {
+        Store.save(items);
+      },
+      deep: true
+    }
+  },
+  methods: {
+    toggle(item) {
+      item.isFinished = !item.isFinished;
+    },
+    addNew() {
+      this.items.push({label: this.newItem, isFinished: false});
+      this.newItem = '';
+    }
+  },
+  components: {}
+}
+</script>
+
+<style>
+.container {
+  text-align: center;
+}
+input {
+  box-shadow: 0 0 8px rgb(150, 104, 219);
+  border: none;
+}
+button {
+  border-radius: 14px;
+  background-color: rgba(0, 0, 0, .1);
+  color: black;
+  box-shadow: 0 0 2px rgb(70, 120, 231);
+}
+ul {
+  text-align: left;
+  margin-left: 50px;
+}
+.finished {
+  text-decoration: line-through;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
